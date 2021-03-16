044094cc21caf1c79c92f360075021c9502830c3
Archit Rathi <64769555+arath089@users.noreply.github.com>
2021-03-16 12:18:19 -0500

    Adding an alert for mobile screens

--- Battleship.js
+++ Battleship.js
@@ -188,3 +188,13 @@ document.querySelector('#play')
 
 document.querySelector('.modal-close span')
     .addEventListener('click', toggleModal);
+
+
+window.onload=function(){
+    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+    if (mobile) {
+            alert("Visit this on a Computer for Better View");              
+    } else {
+    
+    }
+}
